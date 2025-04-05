cf4b0bdb71fadc2dbf803131e868f916e66ca478
Stephen Ostler <stephenjostler@gmail.com>
2025-04-05 22:36:39 +0100

    Added Newsletter Sign Up

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,20 @@
 import FeaturedProject from "../components/FeaturedProject";
 import todoImage from "../assets/placeholder.png";
+import mailImage from "../assets/form.jpg";
 
 function Projects() {
   return (
     <div className="max-w-6xl mx-auto px-4 py-12">
       <h1 className="text-4xl font-bold mb-8 text-center">My Projects</h1>
+
+      <FeaturedProject
+        title="Newsletter Sign Up"
+        description="A form for subscribing to a mail list."
+        tech="CSS, HTML, Javascript"
+        github="https://github.com/steveostler/newsletter-sign-up"
+        live="https://steveostler.github.io/newsletter-sign-up/"
+        image={mailImage}
+      />
 
       <FeaturedProject
         title="To-Do List App"
@@ -13,6 +23,7 @@ function Projects() {
         github="https://github.com/yourusername/todo-app"
         live="https://steveostler.netlify.app/"
         image={todoImage}
+        reverse
       />
 
       <FeaturedProject
@@ -22,7 +33,7 @@ function Projects() {
         github="https://github.com/steveostler/portfolio"
         live="https://steveostler.netlify.app/"
         image={todoImage}
-        reverse
+        
       />
     </div>
   );
